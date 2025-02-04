3f3b276306cf561959eb3a4986a406c26d6d3eff
Isma'il Amin <ismail.amin@ucdenver.edu>
2025-02-04 14:31:59 -0700

    input functions for graph updates

--- dashboard.py
+++ dashboard.py
@@ -39,3 +39,34 @@ app.layout = html.Div
     #graph that will display stock data
     dcc.Graph(id = "stock-graph"),
 ])
+
+#define callback to update the graph when user enters new stock ticker
+@app.callback(
+    #update graph output 
+    Output("stock-graph", "figure"),
+
+    #trigger when the submit button is clicked 
+    Input("submit-button", "n_clicks"),
+
+    #get stock ticker input 
+    Input("stock-input", "value")
+)
+
+def update_graph(n_clicks, ticker):
+    """
+    Updates the stock graph when the user submits a new sotck ticker. 
+        - Fetches data for entered ticker
+        - Updates graph 
+    """
+
+    df = fetch_stockData(ticker)
+
+    #error handling for if stock data isn't found
+    if df is None:
+        return px.line(title = f"No data availabale for {ticker}")
+    
+    #create a new line chart with updated stock data
+    fig = px.line(df, x = df.index, y = "Close", title = f"Stock Prices for {ticker}")
+
+    #return updated graph
+    return fig
